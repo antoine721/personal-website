106a2b71674f242e55453e8856af753e4899eb96
antoine721 <antoine.bbrachet@gmail.com>
2023-11-19 22:17:36 +0100

    rendu final
    test W3C validé
    indentations validé
    travail finis

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector(".slider");
-    const slides = document.querySelectorAll(".slide");
-    const prevBtn = document.querySelector(".prev");
-    const nextBtn = document.querySelector(".next");
+  let currentIndex = 0;
 
-    let currentIndex = 0;
-
-    function showSlide(index) {
-        if (index < 0) {
-            currentIndex = slides.length - 1;
-        } else if (index >= slides.length) {
-            currentIndex = 0;
-        } else {
-            currentIndex = index;
-        }
-
-        const newTransformValue = -currentIndex * 100 + "%";
-        slider.style.transform = "translateX(" + newTransformValue + ")";
+  function showSlide(index) {
+    if (index < 0) {
+      currentIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+      currentIndex = 0;
+    } else {
+      currentIndex = index;
     }
 
-    function showNextSlide() {
-        showSlide(currentIndex + 1);
-    }
+    const newTransformValue = -currentIndex * 100 + "%";
+    slider.style.transform = "translateX(" + newTransformValue + ")";
+  }
 
-    function showPrevSlide() {
-        showSlide(currentIndex - 1);
-    }
+  function showNextSlide() {
+    showSlide(currentIndex + 1);
+  }
 
-    nextBtn.addEventListener("click", showNextSlide);
-    prevBtn.addEventListener("click", showPrevSlide);
+  function showPrevSlide() {
+    showSlide(currentIndex - 1);
+  }
 
-    // Optionnel : Ajoutez une fonction pour faire défiler automatiquement les slides
-    // setInterval(showNextSlide, 5000); // Change de slide toutes les 5 secondes (ajustez selon vos besoins)
+  nextBtn.addEventListener("click", showNextSlide);
+  prevBtn.addEventListener("click", showPrevSlide);
+  setInterval(showNextSlide, 5000);
 });
-
